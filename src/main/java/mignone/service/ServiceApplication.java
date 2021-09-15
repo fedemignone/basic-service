a34aba0da5bfac4fd985977d63f776a22b3f22bd
Federico Mignone <fede.mignone@gmail.com>
2021-09-15 20:49:17 -0300

    Pruebas

--- src/main/java/mignone/service/ServiceApplication.java
+++ src/main/java/mignone/service/ServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ServiceApplication.class, args);
+		System.out.println("");
 	}
 
 }
