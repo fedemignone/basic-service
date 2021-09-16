b17f2ba9cfccc845e8c0f13e7960474797ca941d
Federico Mignone <fede.mignone@gmail.com>
2021-09-15 21:13:13 -0300

    Deatils logging with debug mode, and application context on the main class.

--- src/main/java/mignone/service/ServiceApplication.java
+++ src/main/java/mignone/service/ServiceApplication.java
@@ -2,13 +2,17 @@ package mignone.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ServiceApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(ServiceApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(ServiceApplication.class, args);
+		for (String names: applicationContext.getBeanDefinitionNames()) {
+			System.out.println(names);
+		}
 
 	}
 
