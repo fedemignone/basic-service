75bba6ab38a7e1d98a4cb7e10943765af37f50f5
Federico Mignone <fede.mignone@gmail.com>
2021-09-16 15:13:20 -0300

    Add files via upload
    Initializing project

---
+++ src/main/java/rest/service/mignone/MignoneApplication.java
@@ -0,0 +1,13 @@
+package rest.service.mignone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MignoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MignoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rest/service/mignone/MignoneApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.service.mignone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MignoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
