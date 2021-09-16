fee5bb50928a8a24a166206ce36f6a77ecee23b6
Federico Mignone <fede.mignone@gmail.com>
2021-09-16 16:00:54 -0300

    Se agrega una nueva clase y se prueba con Bean.

--- src/main/java/rest/service/mignone/controller/HelloWorldController.java
+++ src/main/java/rest/service/mignone/controller/HelloWorldController.java
@@ -23,4 +23,9 @@ public class HelloWorldController {
         return "Hello World";
     }
 
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+
 }
