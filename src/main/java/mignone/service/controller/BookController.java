9b7ac4b8f7996df1ecebd68b2383b33ac2173fe5
Federico Mignone <fede.mignone@gmail.com>
2021-09-16 14:36:03 -0300

    Se incorpora Spring Boot Dev Tools/.

--- src/main/java/mignone/service/controller/BookController.java
+++ src/main/java/mignone/service/controller/BookController.java
@@ -12,7 +12,7 @@ public class BookController {
 
     @GetMapping("/books")
         public List<Book> getAllBooks(){
-        return Arrays.asList(new Book(1l, "Name1", "Author1"), new Book(2l, "Name2", "Author2"));
+        return Arrays.asList(new Book(1L, "Name12", "Author1"), new Book(2L, "Name2", "Author2"));
     }
 
 }
