c8f73af80f04e8136fa09ddeb5f3806b7a240a28
Federico Mignone <fede.mignone@gmail.com>
2021-09-18 11:07:57 -0300

    Se agregan diferentes tipos de parametros recibidos en el request (Header, Param, etc).

--- src/main/java/rest/service/mignone/versioning/PersonVersioningController.java
+++ src/main/java/rest/service/mignone/versioning/PersonVersioningController.java
@@ -15,4 +15,34 @@ public class PersonVersioningController {
     public Personv2 personV2(){
         return new Personv2(new Name("Bob", "Marley"));
     }
+
+    @GetMapping(path = "person/param", params="version=1")
+    public Personv1 paramV1(){
+        return new Personv1("Bob Charlie");
+    }
+
+    @GetMapping(path = "person/param", params="version=2")
+    public Personv2 paramV2(){
+        return new Personv2(new Name("Bob", "Marley"));
+    }
+
+    @GetMapping(path = "person/header", headers="X-API-VERSION=1")
+    public Personv1 headerV1(){
+        return new Personv1("Bob Charlie");
+    }
+
+    @GetMapping(path = "person/header", headers="X-API-VERSION=2")
+    public Personv2 hedaerV2(){
+        return new Personv2(new Name("Bob", "Marley"));
+    }
+
+    @GetMapping(path = "person/produces", produces="application/vnd.company.app-v1+json")
+    public Personv1 producersV1(){
+        return new Personv1("Bob Charlie");
+    }
+
+    @GetMapping(path = "person/produces", produces="application/vnd.company.app-v2+json")
+    public Personv1 producersV2(){
+        return new Personv1("Bob Charlie");
+    }
 }
