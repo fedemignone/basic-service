6c24854b153d261fb47db074c6a7d4606622b37a
Federico Mignone <fede.mignone@gmail.com>
2021-09-19 13:20:14 -0300

    Se agregan los metodos de busqueda en el repositorio de usuarios.

--- src/main/java/jpa/project/mignone/service/UserRepositoryCommandLineRunner.java
+++ src/main/java/jpa/project/mignone/service/UserRepositoryCommandLineRunner.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 
@@ -19,6 +22,10 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
     public void run(String... arg0) throws Exception {
         User user = new User("jack", "ADMIN");
         userRepository.save(user);
-        log.info("New user is created" + user);
+        Optional<User> userId1 = userRepository.findById(user.getId());
+        log.info("New user is created" + userId1);
+
+        List<User> users = userRepository.findAll();
+        log.info("All users: " + users);
     }
 }
