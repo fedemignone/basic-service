8dbd8d0a0bc0757bc45cf43677dfe1b02946ab77
Federico Mignone <fede.mignone@gmail.com>
2021-09-19 12:35:04 -0300

    Add files via upload

---
+++ src/test/java/jpa/project/mignone/MignoneApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.project.mignone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MignoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
